38ba690a7cf8c006cf6cb7e18a6be4ec011a049b
reylawas <127361912+reylawas@users.noreply.github.com>
2024-10-17 11:49:43 +0800

    Update db_connection.php

--- includes/db_connection.php
+++ includes/db_connection.php
@@ -12,3 +12,26 @@ $conn = mysqli_connect(
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
+
+
+
+// define('DB_HOST', 'localhost');
+// define('DB_NAME', 'user_db');
+// define('DB_USER', 'root');
+// define('DB_PASS', '');
+
+// try {
+ 
+//     $dsn = "mysql:host=" . DB_HOST . ";dbname=" . DB_NAME . ";charset=utf8mb4";
+//     $options = [
+//         PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+//         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+//         PDO::ATTR_EMULATE_PREPARES   => false,
+//     ];
+//     $pdo = new PDO($dsn, DB_USER, DB_PASS, $options);
+// } catch (PDOException $e) {
+  
+//     exit("Failed to connect to database: " . $e->getMessage());
+// }
+
+?>
